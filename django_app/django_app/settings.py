bbadb681d7c98d5096b32cbd76d86688fadb0395
ChaitanyaHoodie <chaitanya1438gupta@gmail.com>
2024-12-11 01:04:47 +0530

    celery setup complete + 3 functions to fetch owner and repo from urls+ function for fetch repo pr file changes+function to fetch the code and decode the contents from the repo pr file

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_BROKER_URL= 'redis://127.0.0.1:6379'
+CELERY_RESULT_BACKEND= 'redis://127.0.0.1:6379'
+CELERY_ACCEPT_CONTENT= ['application/json']
+CELERY_RESULT_SERIALIZER= 'json'
+CELERY_TAST_SERIALIZER= 'json'
+CELERY_RESULT_EXPIRES= 60 * 60 *24
---
+++ django_app/home/utils/github.py
@@ -0,0 +1,28 @@
+import base64
+import requests
+from urllib.parse import urlparse
+
+def get_owner_and_repo(url):
+    passed_url=urlparse(url)
+    path_parts=passed_url.path.strip("/").split("/")
+    if len(path_parts)>=2:
+        owner,repo=path_parts[0],path_parts[1]
+        return owner,repo
+    return None , None
+
+def fetch_pr_files(repo_url,pr_number,github_token=None):
+    owner, repo=get_owner_and_repo(repo_url)
+    url=f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}/files"
+    headers = {"Authorization" : f"token {github_token}"} if github_token else {}
+    response=requests.get(url,headers=headers)
+    response.raise_for_status()
+    return response
+
+def fetch_file_content(repo_url,file_path,github_token):
+    owner, repo = get_owner_and_repo(repo_url)
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
+    headers = {"Authorization": f"token {github_token}"} if github_token else {}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    content= response.json()
+    return base64.b64decode(content,['content']).decode()
