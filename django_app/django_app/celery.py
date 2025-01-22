cd50a2822b71e3243f53bd530d35a8da8a6990c2
ChaitanyaHoodie <chaitanya1438gupta@gmail.com>
2025-01-23 01:13:37 +0530

    Refactor Celery app initialization and add task management endpoints

--- django_app/django_app/celery.py
+++ django_app/django_app/celery.py
@@ -7,13 +7,13 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
 
-app = Celery('proj')
+app = Celery('django_app')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object(f'django.conf:{settings.__name__}', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
@@ -22,3 +22,7 @@ app.autodiscover_tasks()
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+
+
+import eventlet
+eventlet.monkey_patch()
