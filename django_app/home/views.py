ed154c32b2f5b405997666500287701d80e80e17
ChaitanyaHoodie <chaitanya1438gupta@gmail.com>
2025-01-23 01:13:29 +0530

    Add Celery task management endpoints for repository analysis

--- django_app/home/views.py
+++ django_app/home/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .task import analyze_repo_task
+from celery.result import AsyncResult
 
-# Create your views here.
+@api_view(['POST'])
+def start_task(request):
+    repo_url = request.data.get('repo_url')
+    pr_number = request.data.get('pr_number')
+    github_token = request.data.get('github_token')
+    analyze_repo_task.delay(repo_url, pr_number, github_token)
+    return Response({'status': 'Task started', "task_id": analyze_repo_task.request.id})
+
+@api_view(['GET'])
+def task_status(request, task_id):
+    task = AsyncResult(task_id)
+    return Response({'status': task.status, 'result': task.result, 'task_id': task.id, 'task':task})
