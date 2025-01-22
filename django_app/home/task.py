fb190aa5cfd68eec8d4b76ed0da9adaf0f0b27ca
ChaitanyaHoodie <chaitanya1438gupta@gmail.com>
2025-01-23 00:33:05 +0530

    Implement Celery task for GitHub repo analysis and add LLM code analysis function

---
+++ django_app/home/task.py
@@ -0,0 +1,10 @@
+from celery import Celery
+
+
+app= Celery('django_app')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+from home.utils.github import analyze_pr
+
+@shared_task
+def analyze_repo_task(repo_url, pr_number, github_token=None):
+    return analyze_pr(repo_url, pr_number, github_token)
